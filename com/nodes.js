862a48f1304134245eab1517acc699375eb71317
shir0tetsuo <shadowsword@protonmail.com>
2020-12-24 17:37:44 -0500

    Cleaning up and feature allows inspection of other user nodes

--- com/nodes.js
+++ com/nodes.js
@@ -3,36 +3,40 @@ const settings = require('../settings.json')
 exports.run = async (client, message, params, perms) => {
 
   //const tagList = await client.map.findAll({ attributes: ['coordinate','owner_id','silver','gold']})
-  const tagList = await client.map.findAll({ where: { owner_id: message.author.id } })
+  var other = message.mentions.users.first();
+  var poke = message.author.id;
+  if (other) poke = other.id;
+
+  const tagList = await client.map.findAll({ where: { owner_id: poke } })
   Properties = tagList.length
   if (!isNaN(params[0])) {
     let SliceA = (params[0] - 1) * 10;
-    let SliceB = params[0] * 10;
+    var SliceB = params[0] * 10;
     const tagString = tagList.map(t => `\`${t.coordinate}\`, ${t.silver} Silver, ${t.gold} Gold`).slice(SliceA,SliceB).join('\n')
     var Reply = '';
-    Reply += `**__Nodes__**\n`
+    Reply += `\n**__Nodes__**\n`
+    if (SliceB > Properties) SliceB = Properties
     Reply += `${tagString}\nNodes: Page ${params[0]} (${SliceA+1} - ${SliceB} / ${Properties})`
+    if (other) {
+      Reply += ` (${other.id})`
+      message.react('💙')
+      message.author.send(Reply)
+      return
+    }
     message.reply(Reply)
   } else {
     const tagString = tagList.map(t => `\`${t.coordinate}\`, ${t.silver} Silver, ${t.gold} Gold`).slice(0,10).join('\n')
     var Reply = '';
-    Reply += `**__Nodes__**\n`
+    Reply += `\n**__Nodes__**\n`
     Reply += `${tagString}\nNodes: ${Properties}`
+    if (other) {
+      Reply += ` (${other.id})`
+      message.react('💙')
+      message.author.send(Reply)
+      return
+    }
     message.reply(Reply)
   }
-  //MostExpenseSilver = tagList.map(t => `${t.silver} Silver, \`${t.coordinate}\`, ${t.gold} Gold`).sort((a,b) => a - b).reverse().slice(0,5).join('\n')
-  //MostExpensiveGold = tagList.map(t => `${t.gold} Gold, \`${t.coordinate}\`, ${t.silver} Silver`).sort().reverse().slice(0,5).join('\n')
-
-
-
-
-  //const [res,meta] = await client.map.query(`SELECT * FROM map WHERE user_id = ${message.author.id}`)
-  //P = res.map(t => `${t.coordinate} ${t.user_id} ${t.silver} ${t.gold}`).join('\n')
-  //console.log(P)
-  //const tagList = await client.map.findAll({ attributes: ['user_id','silver','gold']}, { where: { user_id: message.author.id }, limit: 10 });
-  //const tagString = tagList.map(t => `${t.user_id}, ${t.silver}, ${t.gold}`).join('\n') || 'Data Err.';
-  //console.log(tagString)
-  //message.channel.send(`List: ${tagString}`);
 }
 
 exports.conf = {
@@ -45,5 +49,5 @@ exports.conf = {
 exports.help = {
   name: 'nodes',
   description: 'Prints owned map nodes.',
-  usage: 'nodes [page]'
+  usage: 'nodes [page] (mention)'
 };
